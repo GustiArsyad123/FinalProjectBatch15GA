40a644a33feaf7986592d7a710e0c9d8a5e4033c
Rahmat Adi Wijaya <adiraffael@gmail.com>
2021-11-25 17:29:17 +0700

    add error handler

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const app = express()
 const port = process.env.PORT || 3000
 
+const errorHandler = require('./middlewares/errorHandler/errorHandler');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,5 +28,7 @@ app.get('/', (req, res) => {
 app.get('*', (req, res, next) => {
     res.send("404 Page Not Found");
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running smoothly on ${port}`));
