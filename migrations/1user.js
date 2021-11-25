424ac0b0a43ec301af30209729c1fd52bd2f439d
Gerry Pratama <gerryajie@gmail.com>
2021-11-25 20:31:07 +0700

    models

--- migrations/1user.js
+++ migrations/1user.js
@@ -10,11 +10,11 @@ module.exports = {
       },
       firstName: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       lastName: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       userName: {
         type: Sequelize.STRING,
