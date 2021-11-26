cfe309b4a539919e11780e8b0057865d9f3c0973
@suriv27121994 <suriv2712194@gmail.com>
2021-11-26 10:36:09 +0800

    add the ingredient.js in models

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,23 +1,26 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Ingredient extends Model {
+  class ingredient extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-     models.Ingredient.hasMany(models.recipe, {
-       foreignKey: "id_ingredient",
-     });
+      models.Ingredient.hasMany(models.recipe, {
+        foreignKey: "id_ingredient",
+      });
 
       // define association here
     }
   }
-  users.init(
+  ingredient.init(
     {
       name: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      unit: DataTypes.STRING,
+      stock: DataTypes.INTEGER,
     },
     {
       sequelize,
@@ -26,5 +29,5 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "ingredient",
     }
   );
-  return Ingredient;
+  return ingredient;
 };
