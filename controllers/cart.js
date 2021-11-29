aa3891048bb9352aeff95d651d9b7335534ad00b
Gerry Pratama <gerryajie@gmail.com>
2021-11-29 22:48:49 +0700

    Google Oauth

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,188 +1,103 @@
 const { user, cart, recipe } = require("../models");
 
-
 class Cart {
-
-  async getDetailUser(req, res, next) {
+  async showCart(req, res, next) {
     try {
-      const userId = req.userData.id
+      const userId = req.userData.id;
+      const { idRecipe } = req.params;
+      const checkUser = await user.findOne({
+        where: { id: userId },
+      });
 
-      const data = await user.findOne({
-        where: { id: +userId },
+      if (checkUser.id !== userId) {
+        return res.status(401).json({
+          success: false,
+          errors: ["You must have permission to Add it."],
+        });
+      }
+
+      const data = await cart.findAll({
+        where: { id_user: +userId },
         attributes: {
-          exclude: ["password", "createdAt", "updatedAt", "deletedAt"],
+          exclude: ["createdAt", "updatedAt", "deletedAt"],
         },
       });
+      if (data.length == 0) {
+        return res
+          .status(404)
+          .json({ success: false, errors: ["Cart is Empty"] });
+      }
 
       res.status(200).json({ success: true, data: data });
     } catch (error) {
-      res.status(500).json({ success: false, errors: ["Internal Server Error"] });
+      res
+        .status(500)
+        .json({ success: false, errors: ["Internal Server Error"] });
     }
   }
 
   async addCart(req, res, next) {
     try {
-        const userId = req.userData.id;
-        const { idRecipe } = req.params
-        const checkUser = await user.findOne({
-          where: { id: userId },
+      const userId = req.userData.id;
+      const { idRecipe } = req.params;
+      const checkUser = await user.findOne({
+        where: { id: userId },
+      });
+
+      if (checkUser.id !== userId) {
+        return res.status(401).json({
+          success: false,
+          errors: ["You must have permission to delete it."],
         });
-  
-        if (checkUser.id !== userId) {
-          return res.status(401).json({
-            success: false,
-            errors: ["You must have permission to delete it."],
-          });
-        }
-
-        await cart.create({
-            id_user: +userId,
-            id_recipe: +idRecipe
-        })
-
-        res.status(200).json({ success: true, message: ["Success add to cart"] });
-
-    } catch (error) {
-      res.status(500).json({ success: false, errors: ["Internal Server Error"] });
-    }
-  }
-
-  async completeSignUp(req, res, next) {
-    try {
-      const userId = req.userData.id
- 
-      const updateData = await user.update(req.body,
-      {
-        where: { id: +userId },
-      });
-
-      const data = await user.findOne({
-        where: {
-          id: +userId,
-        },
-      });
-
-      res.status(201).json({ success: true, message: ["Success Update Data"], data: data });
-
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, errors: ["Internal Server Error"] });
-    }
-  }
-
-  async updateUser(req, res, next) {
-    try {
-      const userId = req.userData.id
-
-      const updateData = await user.update(req.body,
-      {
-        where: { id: +userId },
-      });
-
-      const data = await user.findOne({
-        where: {
-          id: +userId,
-        },
-      });
-
-      res.status(201).json({ success: true, message: ["Success Update Data"] });
-
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, errors: ["Internal Server Error"] });
-    }
-  }
-
-  async updatePassword(req, res, next) {
-    try {
-      const userId = req.userData.id
-      const { password, confirmPassword } = req.body
-      const hashPassword = encodePin(password)
-
-      if(password != confirmPassword){
-        return res.status(400).json({ success: false, errors: ['Password and Confirm Password not match']})
       }
 
-      const updatePassword = await user.update({
-        password: hashPassword,
-      },
-      {
-        where: { id: +userId },
+      await cart.create({
+        id_user: +userId,
+        id_recipe: +idRecipe,
       });
 
-      const data = await user.findOne({
-        where: {
-          id: +userId,
-        },
-      });
-
-      res.status(201).json({ success: true, message: ["Success Update Password"] });
-
+      res.status(200).json({ success: true, message: ["Success add to cart"] });
     } catch (error) {
-      res.status(500).json({ success: false, errors: ["Internal Server Error"] });
+      res
+        .status(500)
+        .json({ success: false, errors: ["Internal Server Error"] });
     }
   }
 
-  async deleteUser(req, res, next) {
+  async deleteCart(req, res, next) {
     try {
-      
-      const userId = req.userData.id
-      const deletedData = await user.destroy({
+      const userId = req.userData.id;
+      const { idRecipe } = req.params;
+      const checkUser = await user.findOne({
+        where: { id: userId },
+      });
+
+      if (checkUser.id !== userId) {
+        return res.status(401).json({
+          success: false,
+          errors: ["You must have permission uncheck it."],
+        });
+      }
+      const deletedData = await cart.destroy({
         where: {
-          id: +userId,
+          id_recipe: +idRecipe,
         },
-        force: true
+        force: true,
       });
 
       if (deletedData.id !== +userId) {
         return res
           .status(404)
-          .json({ success: false, message: ["User has been deleted"] });
+          .json({ success: false, message: ["Recipe has been deleted"] });
       }
 
-      res.status(200).json({ success: true, message: ["Success deleting data"] });
+      res
+        .status(200)
+        .json({ success: true, message: ["Success deleting data"] });
     } catch (error) {
-       res.status(500).json({ success: false, errors: ["Internal Server Error"] });
-    }
-  }
-
-  async login(req, res, next) {
-    try {
-      const email = req.body.email
-      const password = req.body.password
-      
-      const loginUser = await user.findOne({
-        where: {
-          email: email
-        }
-      })
-
-      if (loginUser == null) {
-        return res.status(401).json({
-          success: false,
-          errors: ["Please Input the correct email and password"]
-        })
-      }
-
-      const hashPass = loginUser.dataValues.password
-      const compareResult = compare(password, hashPass)
-
-      if (!compareResult) {
-        return res.status(401).json({
-          success: false,
-          errors: ["Please input the correct email and password"]
-        })
-      }
-
-      const payload = {id: loginUser.dataValues.id, userName: loginUser.dataValues.userName, email: loginUser.dataValues.email}
-      const token = createToken(payload)
-      res.status(200).json({
-        success: true,
-        token: token
-      })
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, errors: ["Internal Server Error"] });
+      res
+        .status(500)
+        .json({ success: false, errors: ["Internal Server Error"] });
     }
   }
 }
--- routes/cart.js
+++ routes/cart.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const { authentication } = require('../middlewares/Auth/authentication');
+const express = require("express");
+const { authentication } = require("../middlewares/Auth/authentication");
 
-const {
-    addCart,
-} = require('../controllers/cart');
+const { addCart, showCart, deleteCart } = require("../controllers/cart");
 
 const router = express.Router();
 
-router.post('/:idRecipe', authentication, addCart);
+router.post("/:idRecipe", authentication, addCart);
+router.get("/", authentication, showCart);
+router.delete("/:idRecipe", authentication, deleteCart);
 
-module.exports = router; 
+module.exports = router;
