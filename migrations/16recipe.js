24e45b1bf38a86c270d3850180802d5245405212
Gerry Pratama <gerryajie@gmail.com>
2021-11-30 14:03:32 +0700

    loop for quantity

--- migrations/16recipe.js
+++ migrations/16recipe.js
@@ -1,64 +1,64 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('recipes', {
+    await queryInterface.createTable("recipes", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       id_category: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       id_user: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       id_type: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       id_ingredient: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
         allowNull: true,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       duration: {
         allowNull: true,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       serving: {
         allowNull: true,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       image: {
         allowNull: true,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
         allowNull: true,
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       direction: {
         allowNull: true,
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       ingredient: {
         allowNull: true,
-        type: Sequelize.JSONB
+        type: Sequelize.JSONB,
       },
       stock: {
         allowNull: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       price: {
         allowNull: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       id_location: {
         type: Sequelize.INTEGER,
@@ -70,75 +70,74 @@ module.exports = {
       // },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       deletedAt: {
         allowNull: true,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
     // id_category foreign key
-    await queryInterface.addConstraint('recipes', {
-      fields: ['id_category'],
-      type: 'foreign key',
-      name: 'custom_fkey_id_catfdfRecipe',
+    await queryInterface.addConstraint("recipes", {
+      fields: ["id_category"],
+      type: "foreign key",
+      name: "custom_fkey_id_catfdfRecipe",
       references: {
         //Required field
-        table: 'categories',
-        field: 'id',
+        table: "categories",
+        field: "id",
       },
-      onDelete: 'cascade',
-      onUpdate: 'cascade',
+      onDelete: "cascade",
+      onUpdate: "cascade",
     });
 
     // id_user foreign key
-    await queryInterface.addConstraint('recipes', {
-      fields: ['id_user'],
-      type: 'foreign key',
-      name: 'custom_fkey_id_usefdfrRecipe',
+    await queryInterface.addConstraint("recipes", {
+      fields: ["id_user"],
+      type: "foreign key",
+      name: "custom_fkey_id_usefdfrRecipe",
       references: {
         //Required field
-        table: 'users',
-        field: 'id',
+        table: "users",
+        field: "id",
       },
-      onDelete: 'cascade',
-      onUpdate: 'cascade',
+      onDelete: "cascade",
+      onUpdate: "cascade",
     });
 
     // id_type foreign key
-    await queryInterface.addConstraint('recipes', {
-        fields: ['id_type'],
-        type: 'foreign key',
-        name: 'custom_fkey_id_typdfdeRecipe',
-        references: {
-          //Required field
-          table: 'types',
-          field: 'id',
-        },
-        onDelete: 'cascade',
-        onUpdate: 'cascade',
-      });
-
-          // id_location foreign key
-    await queryInterface.addConstraint('recipes', {
-      fields: ['id_location'],
-      type: 'foreign key',
-      name: 'custom_fkey_id_locadfdftionRecipe',
+    await queryInterface.addConstraint("recipes", {
+      fields: ["id_type"],
+      type: "foreign key",
+      name: "custom_fkey_id_typdfdeRecipe",
       references: {
         //Required field
-        table: 'locations',
-        field: 'id',
+        table: "types",
+        field: "id",
       },
-      onDelete: 'cascade',
-      onUpdate: 'cascade',
+      onDelete: "cascade",
+      onUpdate: "cascade",
     });
 
+    // id_location foreign key
+    await queryInterface.addConstraint("recipes", {
+      fields: ["id_location"],
+      type: "foreign key",
+      name: "custom_fkey_id_locadfdftionRecipe",
+      references: {
+        //Required field
+        table: "locations",
+        field: "id",
+      },
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('recipes');
-  }
+    await queryInterface.dropTable("recipes");
+  },
 };
